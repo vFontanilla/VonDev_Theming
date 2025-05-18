1c8475aa7855ef28647a25496d0cc401b5650fcc
vFontanilla <vonfontanilla22@gmail.com>
2025-05-18 13:38:19 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+    /**
+     * Main template file
+     *
+     * @package CustomTheme
+     */
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width", user-scalable=no, initial-scale=1.0>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>VonDev Theme</title>
+</head>
+<body>
+    HELLO VonDev Theme
+</body>
+</html>
