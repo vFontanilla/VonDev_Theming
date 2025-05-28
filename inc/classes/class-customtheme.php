1a1108d7459cc04566ab55aaff1401952853811c
vFontanilla <vonfontanilla22@gmail.com>
2025-05-28 23:14:07 +0800

    with dynamic menu

--- inc/classes/class-customtheme.php
+++ inc/classes/class-customtheme.php
@@ -15,7 +15,9 @@ class CUSTOMTHEME {
 
     protected function __construct() {
 
-        //load classes
+        //Load classes
+
+        Menus::get_instance();
 
         Assets::get_instance();
 
@@ -29,6 +31,51 @@ class CUSTOMTHEME {
 
         add_action('wp_enqueue_scripts', [ $this, 'register_styles' ] );
         add_action('wp_enqueue_scripts', [ $this, 'register_scripts' ] );
+        add_action( 'after_setup_theme', [ $this, 'setup_theme' ] );
+    }
+
+    public function setup_theme() {
+        add_theme_support( 'title-tag' );
+
+        add_theme_support( 'custom-logo', [
+            'header-text'   => [ 'site-title', 'site-description' ],
+            'height'        => 100,
+            'width'         => 400,
+            'flex-height'   => true,
+            'flex-width'    => true,
+        ] );
+
+        add_theme_support( 'custom-background', [ 
+            'default-color' => '#fff',
+            'default-image' => '',
+         ] );
+
+        add_theme_support( 'post-thumbnails' );
+
+        add_theme_support( 'customize-selective-refresh-widgets' );
+
+        add_theme_support( 'automatic-feed-links' );
+
+        add_theme_support( 'html5', [
+            'comment-list',
+            'comment-form',
+            'search-form',
+            'gallery',
+            'caption',
+            'script',
+            'style',
+        ] );
+
+        add_editor_style();
+        add_theme_support( 'wp-block-styles' );
+        add_theme_support( 'align-wide' );
+        add_theme_support( 'editor-styles' );
+        add_theme_support( 'responsive-embeds' );
+
+        global $content_width;
+        if ( ! isset( $content_width )) {
+            $content_width = 1240;
+        }
     }
 
     public function register_styles() {
